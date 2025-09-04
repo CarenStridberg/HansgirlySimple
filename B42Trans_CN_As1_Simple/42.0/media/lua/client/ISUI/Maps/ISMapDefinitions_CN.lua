68f152786edcabb5fb88d0e9da9be0b65b90b13e
CarenStridberg <2861313096@qq.com>
2025-09-04 20:33:20 +0800

    Minor changes.

--- B42Trans_CN_As1_Simple/42.0/media/lua/client/ISUI/Maps/ISMapDefinitions_CN.lua
+++ B42Trans_CN_As1_Simple/42.0/media/lua/client/ISUI/Maps/ISMapDefinitions_CN.lua
@@ -1,38 +1,14 @@
+local _initDirectoryStreetData = MapUtils.initDirectoryStreetData
+
 function MapUtils.initDirectoryStreetData(mapUI, directory)
-	local mapAPI = mapUI.javaObject:getAPIv3();
-	local streetsAPI = mapAPI:getStreetsAPI();
-	local file = directory..'/streets.xml';
-
-	local startIndex, endIndex = string.find(file, "Muldraugh, KY");
+	local startIndex, endIndex = string.find(directory, "Muldraugh, KY");
 	if startIndex ~= nil then
 		return;
 	end
-	local startIndex, endIndex = string.find(file, "muldraugh, ky");
+	local startIndex, endIndex = string.find(directory, "muldraugh, ky");
 	if startIndex ~= nil then
 		return;
 	end
 
-	if fileExists(file) then
-		streetsAPI:addStreetData(file);
-	end
-end
-
-function MapUtils.initDirectoryAnnotations(mapUI, directory)
-	local file = directory..'/worldmap-annotations.lua';
-
-	local startIndex, endIndex = string.find(file, "Muldraugh, KY");
-	if startIndex ~= nil then
-		return;
-	end
-	local startIndex, endIndex = string.find(file, "muldraugh, ky");
-	if startIndex ~= nil then
-		return;
-	end
-
-	if fileExists(file) then
-		local annotationFunction = reloadLuaFile(file);
-		if type(annotationFunction) == "function" then
-			annotationFunction(mapUI);
-		end
-	end
+	_initDirectoryStreetData(mapUI, directory)
 end
