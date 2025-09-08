14790972141fdc01c703df6d5136e8b6f4191b6f
CarenStridberg <2861313096@qq.com>
2025-09-08 11:07:57 +0800

    Minor changes.

---
+++ B42Trans_CN_As1_Simple/42.0/media/lua/client/OptionScreens/MapSpawnSelect_Flx.lua
@@ -0,0 +1,83 @@
+require "ISUI/ISScrollingListBox"
+
+function MapSpawnSelect:_fillList()
+	self.listbox:clear()
+	WORLD_MAP = nil
+	self.mapPanel:clear()
+	local spawnSelectImagePyramid = nil
+	self.sortedList = {};
+	self.notSortedList = {};
+
+	local regions = self:getSpawnRegions()
+	if not regions then return end
+	for _,v in ipairs(regions) do
+		local info = getMapInfo(v.name)
+		if info then
+			local item = {};
+			item.name = info.title or "NO TITLE";
+			item.region = v;
+			item.dir = v.name;
+			item.desc = info.description or "NO DESCRIPTION";
+			item.zoomX = info.zoomX
+			item.zoomY = info.zoomY
+			item.zoomS = info.zoomS
+			item.demoVideo = info.demoVideo
+			--self.listbox:addItem(item.name, item);
+			self:checkSorted(item);
+		else
+			local item = {}
+			item.name = v.name;
+			item.region = v;
+			item.dir = "";
+			item.desc = "";
+			item.worldimage = nil;
+			self:checkSorted(item);
+			--self.listbox:addItem(item.name, item);
+		end
+	end
+	--self.listbox:sort()
+	--self:sortList();
+	if #self.listbox.items > 1 then
+        local item = {}
+        item.name = getText("UI_mapspawn_random");
+        item.region = nil;
+        item.dir = "";
+        item.desc = "";
+        item.worldimage = nil;
+        --self.listbox:addItem(item.name, item);
+		table.insert(self.notSortedList, item);
+    end
+	
+	spawnSelectImagePyramid = getMapInfo("Riverside, KY").spawnSelectImagePyramid or getMapInfo("riverside, ky").spawnSelectImagePyramid or nil
+	if spawnSelectImagePyramid then
+		self.mapPanel:setImagePyramid(spawnSelectImagePyramid)
+	else
+		for _,v in ipairs(regions) do
+			local info = getMapInfo(v.name)
+			if info then
+				self.mapPanel:initMapData('media/maps/'..v.name) -- FIXME: order of multiple maps matters
+				for _,dir in ipairs(info.lots) do
+					self.mapPanel:initMapData('media/maps/'..dir) -- FIXME: order of multiple maps matters
+				end
+			end
+		end
+	end
+
+	-- list has been sorted with MapsOrder
+	for i,v in ipairs(self.sortedList) do
+		self.listbox:addItem(v.name, v);
+	end
+
+	for i,v in ipairs(self.notSortedList) do
+		self.listbox:addItem(v.name, v);
+	end
+
+	self:hideOrShowSaveName()
+	self:recalculateMapSize()
+
+	self.mapPanel.shownInitialLocation = false
+end
+
+if getActivatedMods():contains("\\B42Trans_CN") then
+	MapSpawnSelect.fillList = MapSpawnSelect._fillList;
+end
