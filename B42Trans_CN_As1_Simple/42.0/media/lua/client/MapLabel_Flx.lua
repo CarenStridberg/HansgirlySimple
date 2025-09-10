246ab78a98a9ad74a5153ceb03b561cf4f12f593
CarenStridberg <2861313096@qq.com>
2025-09-11 00:01:50 +0800

    Minor change.

--- B42Trans_CN_As1_Simple/42.0/media/lua/client/MapLabel_Flx.lua
+++ B42Trans_CN_As1_Simple/42.0/media/lua/client/MapLabel_Flx.lua
@@ -1,35 +1,40 @@
 local MapLabelEdit = {}
 
 function MapLabelEdit.isAppropriateKey(text)
-	if not text or text == "" then
-		return nil
-	end
-	return text:find("^MapLabel_[^%s<>]+$");	
+    if not text or text == "" then
+        return false
+    end
+    return text:find("^MapLabel_[^%s<>]+$") ~= nil
 end
 
 function MapLabelEdit.applyChanges()
-	if not ISWorldMap_instance then
-		ISWorldMap.ShowWorldMap(0)
-		ISWorldMap_instance:close()
-	end
+    if not ISWorldMap_instance then
+        ISWorldMap.ShowWorldMap(0)
+        ISWorldMap_instance:close()
+    end
 
-	local mapAPI = ISWorldMap_instance.javaObject:getAPIv3()
-	local symAPI = mapAPI:getSymbolsAPIv2()
-    if not mapAPI or not symAPI then return end
-	
-	local indicesToRemove = {}
-	for i = 0, symAPI:getSymbolCount() - 1 do
-		local sym = symAPI:getSymbolByIndex(i)
-		if sym and sym:isText() then
-			local untranslatedKey = sym:getUntranslatedText()
+    local mapAPI = ISWorldMap_instance.javaObject:getAPIv3()
+    if not mapAPI then return end
+    local symAPI = mapAPI:getSymbolsAPIv2()
+    if not symAPI then return end
+    
+    local indicesToRemove = {}
+    for i = 0, symAPI:getSymbolCount() - 1 do
+        local sym = symAPI:getSymbolByIndex(i)
 
-			if untranslatedKey and not MapLabelEdit.isAppropriateKey(untranslatedKey) then
-				table.insert(indicesToRemove, i)
-			end
-		end
-	end
+        if sym and sym:isText() then
+            if sym:isUserDefined() then
+            else
+                local untranslatedKey = sym:getUntranslatedText()
 
-	if #indicesToRemove > 0 then
+                if not MapLabelEdit.isAppropriateKey(untranslatedKey) then
+                    table.insert(indicesToRemove, i)
+                end
+            end
+        end
+    end
+
+    if #indicesToRemove > 0 then
         table.sort(indicesToRemove, function(a, b) return a > b end)
         for _, index in ipairs(indicesToRemove) do
             symAPI:removeSymbolByIndex(index)
