8d1aa78683f75e85ce122180ae7719f72061124e
CarenStridberg <2861313096@qq.com>
2025-12-15 17:24:00 +0800

    v2.51.1

--- B42Trans_CN_As1_Simple/42.0/media/lua/client/ISWidgetRecipeCategories_CN.lua
+++ B42Trans_CN_As1_Simple/42.0/media/lua/client/ISWidgetRecipeCategories_CN.lua
@@ -5,6 +5,8 @@ function ISWidgetRecipeCategories:populateCategoryList()
     for _, item in ipairs(self.recipeCategoryPanel.items) do
         if item.text == "-- ALL --" then
             item.text = getTextOrNull("UI_CraftCat_All") or item.text
+        elseif item.text == "Wall Coverings" then
+            item.text = getTextOrNull("UI_CraftCat_WallCoverings") or item.text
         else
             item.text = getTextOrNull("UI_CraftCat_" .. item.text) or item.text
         end
